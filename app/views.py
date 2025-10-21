d191217e27681cb1133eddc1517fca8c88666638
Riozaki <romagonta@yandex.ru>
2025-10-21 15:14:21 +0300

    index fix (production mode)

--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
+from django.conf import settings
 from django.shortcuts import render
 
-
 def index(request):
-    return render(request, 'app/index.html')
+    return render(request, 'app/index.html', {
+        'debug_mode': settings.DEBUG
+    })
